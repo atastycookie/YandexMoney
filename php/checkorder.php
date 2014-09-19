0e86ddcc61f6d94fcdcbb761b59f8fd5eb2fb374
Roman Ananyev <aTastyCookie@users.noreply.github.com>
2014-09-19 15:18:31 +0400

    Update checkorder.php

--- php/checkorder.php
+++ php/checkorder.php
@@ -4,14 +4,14 @@
 			
 		$shoppassword = 'shoppw';
 		$hash = md5($_POST['action'].';'.$_POST['orderSumAmount'].';'.$_POST['orderSumCurrencyPaycash'].';'.$_POST['orderSumBankPaycash'].';'.$_POST['shopId'].';'.$_POST['invoiceId'].';'.$_POST['customerNumber'].';'.$shoppassword);		
-		if (strtolower($hash) != strtolower($_POST['md5'])) { 
+		if (strtolower($hash) != strtolower($_POST['md5'])){
 			$code = 1;
 		}
 		else {
 			$code = 0;
-			print '<?xml version="1.0" encoding="UTF-8"?>';
-			print '<checkOrderResponse performedDatetime="'. $_POST['requestDatetime'] .'" code="'.$code.'"'. ' invoiceId="'. $_POST['invoiceId'] .'" shopId="'. $_POST['shopId'] .'"/>';
 		}
+		print '<?xml version="1.0" encoding="UTF-8"?>';
+		print '<checkOrderResponse performedDatetime="'. $_POST['requestDatetime'] .'" code="'.$code.'"'. ' invoiceId="'. $_POST['invoiceId'] .'" shopId="'. $_POST['shopId'] .'"/>';
 	}
 
 ?>
